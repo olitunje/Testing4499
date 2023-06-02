891f65a828a5fc02e938434a4a09b275e9691eb2
steven <steven@DESKTOP-G064I81>
2023-06-02 09:14:12 +0200

    Projektdateien hinzufügen.

---
+++ Testing4499/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DesignerishMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Designerish Menu";
+
+            string[] options = { "Option 1", "Option 2", "Option 3", "Option 4", "Option 5" };
+            int selectedOption = 0;
+
+            ConsoleKey key;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("=== Designerish Menu ===\n");
+
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (i == selectedOption)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(">> " + options[i]);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine(options[i]);
+                    }
+                }
+
+                key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        if (selectedOption > 0)
+                            selectedOption--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (selectedOption < options.Length - 1)
+                            selectedOption++;
+                        break;
+                }
+            } while (key != ConsoleKey.Enter);
+
+            Console.Clear();
+            Console.WriteLine("You selected: " + options[selectedOption]);
+            Console.ReadLine();
+        }
+    }
+}
